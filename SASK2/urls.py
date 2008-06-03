059c18a189bf49de335bb6e7e8a9c443291c4390
bricec <bricec@d8757e53-e53d-0410-a107-332ea49df6b6>
2008-06-03 15:13:59 +0000

    ajax species search form

--- SASK2/urls.py
+++ SASK2/urls.py
@@ -3,27 +3,11 @@ from SASK2 import settings
 
 urlpatterns = patterns(
      '', 
-	(r'^admin/', include('django.contrib.admin.urls')),              
-    (r'^SASK/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'} ),
-    (r'^SASK/home$', 'home.views.home' ),
-
-    (r'^SASK/trees/$', 'trees.views.home' ),
-    (r'^SASK/trees/add$', 'trees.views.add' ),
-    (r'^SASK/trees/edit$', 'sites.views.edit' ),
-    (r'^SASK/trees/export$', 'trees.views.export' ),
-
-    (r'^SASK/sites/$', 'sites.views.home' ),
-    (r'^SASK/sites/$', 'sites.views.home' ),
-    (r'^SASK/trees/$', 'trees.views.home' ),
-    (r'^SASK/sites/$', 'sites.views.home' ),
-    (r'^SASK/trees/$', 'trees.views.home' ),
-    (r'^SASK/sites/$', 'sites.views.home' ),
-    (r'^trees/Enter/home/', 'trees.views.enter' ),
-    (r'^trees/Enter/new/', 'trees.views.new'),
-    (r'^trees/Enter/selectSite/', 'trees.views.selectSite'),
-    (r'^trees/Enter/tree/(?P<recievedSite>.+)/', 'trees.views.treedata'),
-    (r'^trees/Export/$', 'trees.views.selectSiteToExport' ),
-    (r'^trees/Export/(?P<recievedSite>.+)/', 'trees.views.exportTreedata2'),
+    (r'^species/search/$', 'SASK2.species.views.search'), 
+    (r'^species/add/$', 'SASK2.species.views.add'),      
+    (r'^species/list/$', 'SASK2.species.views.list'),      
+    #(r'^SASK/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'} ),
+    
                        
     # Uncomment this for admin:
     (r'^admin/', include('django.contrib.admin.urls')),
