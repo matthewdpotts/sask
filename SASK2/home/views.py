c7b97834b2dd6c02386b146592543f989364d3aa
bricec <bricec@d8757e53-e53d-0410-a107-332ea49df6b6>
2008-06-09 14:24:23 +0000

    delete SASK2 folder

--- SASK2/home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import get_object_or_404, render_to_response
-from django.http import Http404, HttpResponse, HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def home(request):
-    return render_to_response('home.html')
