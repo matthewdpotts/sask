4d1a7528769f33e9a662c1e4ef4feeae39442d8f
bricec <bricec@d8757e53-e53d-0410-a107-332ea49df6b6>
2008-06-18 03:12:03 +0000

    Add Survey, Add Second Survey, Add Tree with hidden Add Species

--- SASK2/species/views.py
+++ SASK2/species/views.py
@@ -4,9 +4,23 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from SASK2.trees.models import Species, TreeSpecies
 from SASK2.species.forms import SpeciesForm
+from django.http import HttpResponse
 
 def Home(request):
 	return render_to_response("species/home.html")
+
+def GetSpeciesForm(request):
+	if request.method == 'POST' and request.POST.get('BlankFormRequest','') != 'True':
+		speciesform = SpeciesForm(data=request.POST)
+		if speciesform.is_valid():
+			species = speciesform.save()
+			response = '\'%s\',\'%s\'' % (species,species.id)
+			return HttpResponse(response)
+		else:
+			return render_to_response("species/SpeciesForm.html",{'form':speciesform,'Message':'Please check the form for errors.'})
+			
+	else:
+		return render_to_response("species/SpeciesForm.html",{'form':SpeciesForm()})
 
 def search(request):
 	query = request.POST.get('q', '')
--- SASK2/trees/views.py
+++ SASK2/trees/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render_to_response
 from SASK2.trees.models import Tree, TreeSurvey
-from SASK2.trees.forms import TreeForm, TreeSurveyForm
+from SASK2.trees.forms import * 
 from django.core.urlresolvers import reverse
+from django.core import serializers
+from django.http import HttpResponse
 
-def GetNewTreeSurveyForm(request):
+def GetTreeSurveyForm(request):
 	Message = ''
-	if request.method == 'POST':
+	if request.method == 'POST' and request.POST.get('BlankFormRequest',False)==False:
 		treeform = TreeForm(data = request.POST)
 		unlinkedtreesurveyform = UnlinkedTreeSurveyForm(data = request.POST)
 		if treeform.is_valid() and unlinkedtreesurveyform.is_valid():
-			tree = treeform.save()
+			tree = treeform.save(commit = False)
+			Matches = Tree.objects.filter(tag = tree.tag)
+			if len(Matches) > 0:
+				treeform.errors['tag'] = ('A tree with this tag number has already been added.',)
+				Message += 'Please check the data for errors.'
+				return render_to_response('trees/NewTreeSurveyForm.html',{'TreeForm':treeform,'TreeSurveyForm':unlinkedtreesurveyform})
+			tree.save()
 			treesurvey = unlinkedtreesurveyform.save(commit = False)
 			treesurvey.tree = tree
 			treesurvey.save()
-			return render_to_response('trees/NewTreeSurveyForm.html',{'Tree':tree,'TreeSurvey':TreeSurvey})
+			Message += 'Tree (%s) and TreeSurvey (%s) successfully added' % (tree,treesurvey)
+			return render_to_response('trees/NewTreeSurveyForm.html',{'Tree':tree,'TreeSurvey':treesurvey})
 		else:
 			Message += 'Please check the data for errors.'
 			return render_to_response('trees/NewTreeSurveyForm.html',{'TreeForm':treeform,'TreeSurveyForm':unlinkedtreesurveyform,'Message':Message})
@@ -24,3 +33,32 @@ def GetNewTreeSurveyForm(request):
 
 def NewTreeSurvey(request):
 	return render_to_response('trees/NewTreeSurvey.html')
+
+def SecondTreeSurvey(request):
+	Entries = []
+	AllTrees = Tree.objects.all()
+	for tree in AllTrees:
+		Entry = [tree,]
+		TwoSurveys = TreeSurvey.objects.all().filter(tree=tree).order_by('date')[:2]
+		Entry.extend(TwoSurveys)
+		#Entry[1] = TreeSurveyForm(instance=Entry[1])
+		if len(Entry) == 3:
+			Entry[2] = TreeSurveyForm(instance=Entry[2])
+		else:
+			treesurvey = TreeSurvey(tree = Entry[0])
+			Entry.append(TreeSurveyForm(instance=treesurvey))		
+		Entries.append(Entry)
+
+	return render_to_response('trees/SecondTreeSurvey.html', {'Entries':Entries})
+
+def SubmitTreeSurveyForm(request):
+	treesurveyform = TreeSurveyForm(data=request.POST)
+	if treesurveyform.is_valid():
+		treesurvey = treesurveyform.save()
+		return HttpResponse('%s' % (treesurvey.tree.tag,))
+	else:
+		Errors = 'The survey couldn\'t be saved. Please check the following fields for errors:\n'
+		for tag in treesurveyform.errors:
+			Errors = '%s%s\n' % (Errors,tag)
+		return HttpResponse(Errors)
+		#return render_to_response('trees/TreeSurveyForm.html',{'form':treesurveyform});
