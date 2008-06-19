730a0726625e86f30aa639f2abfbe3a68b38a4af
bricec <bricec@d8757e53-e53d-0410-a107-332ea49df6b6>
2008-06-19 06:15:54 +0000

    redumped fixtures to reflect model change

--- trunk/SASK2/settings/settings.py
+++ trunk/SASK2/settings/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'postgresql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'SASK'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'brice'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'password'         # Not used with sqlite3.
+DATABASE_USER = 'dbuser'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'dbpassword'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
