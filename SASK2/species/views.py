a7f5bed5bb29739174869d451f707287c17a0659
bricec <bricec@d8757e53-e53d-0410-a107-332ea49df6b6>
2008-06-20 06:07:10 +0000

    species edit

--- SASK2/species/views.py
+++ SASK2/species/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from SASK2.trees.models import Species, TreeSpecies
-from SASK2.species.forms import SpeciesForm
+from SASK2.species.forms import * 
 from django.http import HttpResponse
 
 def Home(request):
@@ -80,20 +80,28 @@ def AddSpecies(request):
 		Form = SpeciesForm(instance=SpeciesInstance)
 	return render_to_response('add.html',{'Form':Form, 'Message':Message,'Title':Title,'PageTitle':PageTitle,'ButtonLabel':ButtonLabel,'url':reverse("AddSpecies")})
 
-def EditSpecies(request, SpeciesID=''):
-        SpeciesID = int(SpeciesID)
-        Title = PageTitle = "Edit Species Information"
-        Message = ''
-        if request.method == 'POST':
-                sf = SpeciesForm(data = request.POST)
-                if sf.is_valid():
-                        s = sf.save(commit=False)
-                       	s.id = SpeciesID
-                        s.save()
-                        Message += 'The edit was successfully made.'
-                else:
-                        Message += 'Please check the data for errors.'
-        else:
-                species = Species.objects.get(id=SpeciesID)
-                sf = SpeciesForm(instance=species)
-        return render_to_response('species/MakeEdit.html',{'Title':Title,'PageTitle':PageTitle,'Form':sf,'Message':Message})
+def EditSpecies(request):
+        return render_to_response('species/EditSpecies.html')
+
+def SpeciesSelectorForm(request):
+	family = request.POST.get('family','')
+	genus = request.POST.get('genus','')
+	species = request.POST.get('species','')
+	spcode = request.POST.get('spcode','')
+	ssf = SpeciesSelectorFormForm(family=family, genus=genus, species=species, spcode=spcode)
+	return render_to_response('species/SpeciesSelector.html',{'form':ssf})
+
+def EditSpeciesForm(request, SpeciesID=''):
+	if request.method == 'POST':
+		sf = SpeciesForm(data = request.POST)
+		if sf.is_valid():
+			s = sf.save(commit=False)
+			s.id = SpeciesID
+			s.save()
+			Message += 'The edit was made successfully.'
+		else:
+			Message += 'Please check the data for errors'
+	else:
+		s = Species.objects.get(id = SpeciesID)
+		sf = SpeciesForm(instance=sf)
+	return render_to_response('species/SpeciesForm.html',{'form':sf,'Message':Message})
