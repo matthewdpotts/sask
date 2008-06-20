c2afd99f621f3da473812b68ae2014a5a102d1ad
bricec <bricec@d8757e53-e53d-0410-a107-332ea49df6b6>
2008-06-20 06:07:10 +0000

    species edit

--- trunk/SASK2/species/forms.py
+++ trunk/SASK2/species/forms.py
@@ -5,3 +5,28 @@ class SpeciesForm(forms.ModelForm):
 	
 	class Meta:
 		model=Species
+
+class SpeciesSelectorFormForm(forms.Form):
+	def __init__(self, *args, **kwargs):
+		family = kwargs.pop('family', False)
+		genus = kwargs.pop('genus', False)
+		species = kwargs.pop('species', False)
+		spcode = kwargs.pop('spcode', False)
+		#del kwargs['family']
+		#del kwargs['genus']
+		#del kwargs['species']
+		#del kwargs['spcode']
+		super(SpeciesSelectorFormForm, self).__init__(*args, **kwargs)
+		qs = Species.objects.all()
+		if family:
+			qs = qs.filter(family__startswith=family)
+		if genus:
+			qs = qs.filter(genus__startswith=genus)
+		if species:
+			qs = qs.filter(species__startswith=species)
+		if spcode:
+			qs = qs.filter(spcode__startswith=spcode)
+		qs = qs.distinct()
+		self.fields['species'].queryset = qs
+
+	species = forms.ModelChoiceField(Species.objects.all())
